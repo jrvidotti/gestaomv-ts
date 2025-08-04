32d2c253388ac43c758f2985bd9c9d691136ebf0
Junior Vidotti <junior@modaverao.com.br>
2025-08-04 09:22:06 -0400

    fmt

--- gestaomv/drizzle.config.ts
+++ gestaomv/drizzle.config.ts
@@ -1,10 +1,10 @@
-import type { Config } from 'drizzle-kit';
+import type { Config } from "drizzle-kit";
 
 export default {
-  schema: './src/db/schemas.ts',
-  out: './src/db/migrations',
-  dialect: 'sqlite',
-  dbCredentials: {
-    url: process.env.DATABASE_PATH || './data/database.sqlite',
-  },
+	schema: "./src/db/schemas.ts",
+	out: "./src/db/migrations",
+	dialect: "sqlite",
+	dbCredentials: {
+		url: process.env.DATABASE_PATH || "./data/database.sqlite",
+	},
 } satisfies Config;
